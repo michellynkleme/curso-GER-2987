15cc5682161cff6a7b31e7df153eb806d5dae324
michellynkleme <michelly.narita@lemeforense.com.br>
2023-03-23 11:58:45 -0300

    Aula #5 Yii2 - Banco de dados
    Prática do video-aula #5.

--- projectyii/config/db.php
+++ projectyii/config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=172.16.100.107;port=5432;dbname=michelly',
+    'username' => 'michelly',
+    'password' => 'michelly123',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    //'schemaCache' => 'cache', pgsql
 ];
--- projectyii/controllers/ExerciciosController.php
+++ projectyii/controllers/ExerciciosController.php
@@ -3,8 +3,11 @@
 namespace app\controllers;
 
 use app\models\CadastroModel;
+use app\models\Pessoas;
 use yii\web\Controller;
 use Yii;
+use yii\data\Pagination;
+
 class ExerciciosController extends Controller
 {
     public function actionFormulario()
@@ -26,4 +29,36 @@ class ExerciciosController extends Controller
        
         }
     }
-}
+
+    public function actionPessoas()
+    {
+        /* teste: $pessoas = Pessoas::find()->orderBy('nome')->all();
+        echo '<pre>'; print_r($pessoas);*/
+
+        /* teste: $pessoa = Pessoas::findOne(5);
+        echo $pessoa->nome." - ".$pessoa->email;
+
+        $pessoa = Pessoas::findOne(5);
+        $pessoa->nome = 'Clovis Nogueira Brito';
+        $pessoa->save();*/
+
+        $query = Pessoas::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' =>3,
+            'totalCount' => $query->count()
+        ]);
+
+        $pessoas = $query->orderBy('nome')
+                            ->offset($pagination->offset)
+                            ->limit($pagination->limit)
+                            ->all();
+
+        return $this->render('pessoas', [
+            'pessoas' => $pessoas,
+            'pagination' => $pagination
+        ]);
+
+    }
+
+    }

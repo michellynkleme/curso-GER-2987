d8ef7e0e3283dba763abe6ad1fd7853404086aae
michellynkleme <michelly.narita@lemeforense.com.br>
2023-03-23 10:06:18 -0300

    Aula #4 Yii2 - Formulários
    Prática do video-aula #4
    (O video-aula 3 é focado em instalar o projeto com composer, o que  não é possível ser comitado)

---
+++ projectyii/views/exercicios/formulario-confirmacao.php
@@ -0,0 +1,9 @@
+<?php ?>
+
+<p>Você inseriu as seguintes informações no formulário:</p>
+
+<ul>
+    <li><label>Nome</label>: <?= $model->nome ?></li>
+    <li><label>E-mail</label>: <?= $model->email ?></li>
+    <li><label>Idade</label>: <?= $model->idade ?></li>
+</ul>
